bf0df5bdeb7127b3a958f260b168098fa2116dba
Jessica Godwin <jlg0003@uw.edu>
2025-08-27 21:24:54 -0700

    Update master script after renaming pipeline files.

--- GI master script.R
+++ GI master script.R
@@ -12,16 +12,14 @@ rm(list=ls())
 set.seed(1)
 
 # Prepare project files
-source("SOGI_00_packages.R") # call in packages; you may have to open this file and install new packages 
-source("SOGI_01_generate_BRFSS_index_variable.R") # !! SLOW !! creates manageable versions of BRFSS data (.rds rather than .XPT) - only run once to generate these files
-source("SOGI_02_prepare_BRFSS.R") # run this file to prepare a single file `brfss_final.rds`
+source("00_packages.R") # call in packages; you may have to open this file and install new packages 
+source("01_BRFSS_index_variable.R") # !! SLOW !! creates manageable versions of BRFSS data (.rds rather than .XPT) - only run once to generate these files
+source("02_prepare_BRFSS.R") # run this file to prepare a single file `brfss_final.rds`
 
 # GI Analyses
-source("SOGI_21_GI_prepare.R") # prepare objects for GI analyses
-source("SOGI_22_GI_analyses.R") # GI analyses
-source("SOGI_21_GI_design_based.R") # Get design based prev estimates
-source("SOGI_22_GI_Q1.R") # Fit null, A, P, C, AP, AC, PC models
-source("SOGI_22_GI_Q2_mAPC.R") # Fit Bayesian mAPC models for tw, tm, nbgnc
-source("SOGI_22_GI_Q3.R")
-source("SOGI_22_GI_Q4.R") 
-source("SOGI_22_GI_Q5.R")
+source("21_survey_weighted.R") # Get design based prev estimates
+source("22_descriptive_plots.R") # Generage Figs 1, S1, and S2
+source("23_analysis_1.R") # Fit null, A, P, C, AP, AC, PC models for tw, tm, nbgnc
+source("24_analysis_2.R") # Fit Bayesian mAPC models for tw, tm, nbgnc
+source("25_analysis_3.R") # Fit null, A, P, C, AP, AC, PC models for dkns, dta
+source("26_analysis_4.R") # Sex, Sex assigned at birth, and So by GI
