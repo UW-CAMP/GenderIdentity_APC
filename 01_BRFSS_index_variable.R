481d739c1cb5f1a7230e0dba577bdaa9ec5e153e
Jessica Godwin <jlg0003@uw.edu>
2025-08-27 20:35:03 -0700

    Add preamble, clean up headings for 01.R

--- 01_BRFSS_index_variable.R
+++ 01_BRFSS_index_variable.R
@@ -1,20 +1,32 @@
-### DEPRECATED
+## Code loading packages necessary for all analyses of the following manuscript:
+##
+##    Barry MP, Godwin J, Lucas R, Tordoff DM, Koenig LJ, Goodreau SM. 2025.
+##      Demographic trends in gender identity among adults in the United States,
+##      2014-2021. International Journal of Transgender Health. 
+##      Online first: https://www.tandfonline.com/doi/full/10.1080/26895269.2025.2537874.
+## 
+##  Script authors: Barry MP, Godwin J, Goodreau SM
+##
+##  This script takes large .XPT BRFSS files, assigns index numbers
+##  to each BRFSS observation, and generates .rds files for input to 
+##  02_prepare_BRFSS.R
+##
+##  Inputs:
+##    - data - raw/LLCP20YY.XPT for YY in 14:21
+##
+##   Outputs:
+##    - data - clean/brfss.rds
+##    - data - clean/brfssYY.rds for YY in 14:21
 
-### This script takes large .XPT BRFSS files, assigns index numbers to each BRFSS observation, 
-#          and generates .rds files for later use in the SOGI analysis.
-
-### Prepare workspace ----- 
-# clear environment
+# Setup ####
 rm(list = ls())
 
-# * call in packages ----
-source("SOGI_00_packages.R")
-
-# * define functions -----
+## Functions ####
 tableNA <- function(x, ...){
    table(x, useNA = "ifany", ...)  
 }
 
+# Load raw BRFSS ####
 brfss <- list()
 for (yr in 14:21) {
    cat('Loading year', yr, '\n')
@@ -24,6 +36,7 @@ for (yr in 14:21) {
    brfss[[yr]]$index <- as.numeric(paste0(1:nrow(brfss[[yr]]), yr))
 }
 
+# Save outputs ####
 write_rds(brfss, "data - clean/brfss.rds")
 write_rds(brfss[[14]], "data - clean/brfss14.rds")
 write_rds(brfss[[15]], "data - clean/brfss15.rds")
